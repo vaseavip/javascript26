3fa9ba128f144dd700e25f1160f66d3d246d1cff
vaseavip <perjuvasile0@gmail.com>
2026-02-12 18:13:52 +0100

    primul

--- javascript_modul1/javascript/main.js
+++ javascript_modul1/javascript/main.js
@@ -1,6 +1,13 @@
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-  if (i % 2 === 0) {
-    console.log(i);
+function myFunction(numar) {
+  if (numar === 'gold') {
+    console.log('Felicitari!EstiMembruGold!');
+  } else if (numar === 'silver') {
+    console.log('Felicitari! Esti Membru Silver!');
+  } else {
+    console.log('Ne pare rau! Nu esti membru!');
   }
 }
+
+myFunction('gold');
+myFunction('silver');
+myFunction('bronze');
