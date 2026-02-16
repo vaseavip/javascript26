23b3480ebe6f30a4764373032bf067cdc44e3774
vaseavip <perjuvasile0@gmail.com>
2026-02-16 17:27:00 +0100

    primul

--- javascript_modul1/javascript/main.js
+++ javascript_modul1/javascript/main.js
@@ -1,13 +1,29 @@
-function myFunction(numar) {
-  if (numar === 'gold') {
-    console.log('Felicitari!EstiMembruGold!');
-  } else if (numar === 'silver') {
-    console.log('Felicitari! Esti Membru Silver!');
-  } else {
-    console.log('Ne pare rau! Nu esti membru!');
+function getDayType(day) {
+  switch (day) {
+    case 'Luni':
+    case 'Marti':
+    case 'Miercuri':
+    case 'Joi':
+    case 'Vineri':
+      return 'Zi lucrătoare';
+
+    case 'Sambata':
+    case 'Duminica':
+      return 'Weekend';
+
+    default:
+      return 'Zi invalidă';
   }
 }
+console.log(getDayType('Luni')); // Zi lucrătoare
+console.log(getDayType('Sambata'));
 
-myFunction('gold');
-myFunction('silver');
-myFunction('bronze');
+let total = 500;
+let shipping = total > 500 ? 'Livrare gratuită' : 'Cost de livrare: 50 lei';
+console.log(shipping); // Livrare gratuită
+
+function checkAge(age) {
+  return age >= 18 ? 'Ești major' : 'Ești minor';
+}
+console.log(checkAge(20)); // Ești major
+console.log(checkAge(16)); // Ești minor
